6695513c39df09e7740c8e0eb88da7ff15c25f87
lio <licroning@163.com>
2022-08-30 17:58:30 +0800

    chore: lint

--- App.js
+++ App.js
@@ -1,14 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +8,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# React Native Starter
+
+A Better React Native Starter Template
